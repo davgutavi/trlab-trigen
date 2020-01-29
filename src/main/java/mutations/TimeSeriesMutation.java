17ceb890fa7fe3f7317424531f0b23a9b567d9ca
davgutavi <davgutavi@gmail.com>
2020-01-29 17:40:48 +0100

    Alentejo#4.1

---
+++ src/main/java/mutations/TimeSeriesMutation.java
@@ -0,0 +1,64 @@
+package mutations;
+
+import algcore.AlgorithmIndividual;
+import algcore.Mutation;
+import algutils.AlgorithmRandomUtilities;
+import strmutation.CondicionInsertionStrategy;
+import strmutation.ConditionChangeStrategy;
+import strmutation.ConditionRemovalStrategy;
+import strmutation.GenChangeStrategy;
+import strmutation.GenInsertionStrategy;
+import strmutation.GenRemovalStrategy;
+//import strmutation.TimeChangeStrategy;
+//import strmutation.TimeInsertionStrategy;
+//import strmutation.TimeRemovalStrategy;
+
+public class TimeSeriesMutation implements Mutation {
+
+//	private MutationStrategy quitar_tiempo;
+	private MutationStrategy quitar_gen;
+	private MutationStrategy quitar_condicion;
+//	private MutationStrategy poner_tiempo;
+	private MutationStrategy poner_gen;
+	private MutationStrategy poner_condicion;
+	private MutationStrategy cambiar_gen;
+	private MutationStrategy cambiar_condicion;
+//	private MutationStrategy cambiar_tiempo;
+		
+	public TimeSeriesMutation() {
+
+//		quitar_tiempo    = new TimeRemovalStrategy();
+		quitar_gen       = new GenRemovalStrategy();
+		quitar_condicion = new ConditionRemovalStrategy();
+//		poner_tiempo     = new TimeInsertionStrategy();
+		poner_gen        = new GenInsertionStrategy();
+		poner_condicion  = new CondicionInsertionStrategy();
+		cambiar_gen = new GenChangeStrategy();
+		cambiar_condicion = new ConditionChangeStrategy();
+//		cambiar_tiempo = new TimeChangeStrategy();
+
+	}
+
+	public void mutate(AlgorithmIndividual individual) {
+
+		AlgorithmRandomUtilities randomSupport = AlgorithmRandomUtilities.getInstance();
+		
+		applyAlteration (individual,randomSupport.getFromInterval(1,9));
+
+	}
+
+	private void applyAlteration(AlgorithmIndividual individual, int type) {
+		
+		if      (type==1) quitar_gen.alter(individual);
+		else if (type==2) quitar_condicion.alter(individual);
+//		else if (type==3) quitar_tiempo.alter(individual);
+		else if (type==4) poner_gen.alter(individual);
+		else if (type==5) poner_condicion.alter(individual);
+//		else if (type==6) poner_tiempo.alter(individual);	
+		else if (type==7) cambiar_gen.alter(individual);
+		else if (type==8) cambiar_condicion.alter(individual);
+//		else if (type==9) cambiar_tiempo.alter(individual);	
+				
+	}
+
+}
